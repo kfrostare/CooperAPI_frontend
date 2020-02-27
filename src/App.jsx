f1bcb13668cdf1636ae72ad90f3cb21286c4bf20
karolina <karolina.frostare@gmail.com>
2020-02-27 14:56:15 +0100

    cleans out app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import React, { Component } from 'react';
+
 import DisplayCooperResult from "./components/DisplayCooperResult";
 import InputFields from './components/InputFields';
 
 class App extends Component {
   state = {
-    distance:"",
+    distance: "",
     gender: "female",
     age: ""
   };
 
   onChangeHandler = e => {
-    this.setState({ [e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
     return (
       <>
-        <label>Distance</label>
-        <input
-          onChange={this.onChangeHandler}
-          name="distance"
-          id="distance"
-        ></input>
-
-        <select onChange={this.onChangeHandler} name="gender" id="gender">
-          <option value="female">Female</option>
-          <options value="male">Male</options>
-        </select>
-
-        <label>Age</label>
-        <input onChange={this.onChangeHandler} name="age" id="age"></input>
+        <InputFields onChangeHandler={this.onChangeHandler} />
         <DisplayCooperResult
-          distance={this.state.distance}
-          gender={this.state.gender}
-          age={this.state.age}
+        distance={this.state.distance}
+        gender={this.state.gender}
+        age={this.state.age}
         />
       </>
     );
