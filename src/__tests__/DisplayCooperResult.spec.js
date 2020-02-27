fa8d8e93a6e0840b2b1d8a85d89d4d3c7e71715f
Craft Academy <craftacademy@Crafts-MacBook-Air.local>
2020-02-27 13:19:53 +0100

    updates dusplaycoopertests folder

--- src/__tests__/DisplayCooperResult.spec.js
+++ src/__tests__/DisplayCooperResult.spec.js
@@ -4,44 +4,40 @@ import { shallow } from "enzyme";
 import DisplayCooperResult from "../components/DisplayCooperResult";
 
 describe("<DisplayCooperResult />", () => {
-    let describedComponent;
-    describe("evaluates the correct result for female/poor", () => {
-        beforeAll(() => {
-            describedComponent = shallow(
-                <DisplayCooperResult distance="1000" gender="feamle" age="23" />
-            
-            );
-        });
-    
-        it("and returns the assessment", () => {
-            expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Poor");
-        });
-
-        it("and returns the data user put in", () => {
-            expect(describedComponent.find("p#cooper-message").text()).toEqual(
-                "23 y/o female running 1000 meters."
-            );
-        });
+  let describedComponent;
+  describe("evaluates the correct result for female/poor", () => { 
+    beforeAll(() => {
+      describedComponent = shallow(
+        <DisplayCooperResult distance="1000" gender="female" age="23" />
+      );
     });
 
+    it("and returns the assessment", () => {
+      expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Poor");
+    });
 
-    describe("evaluates the correct result for female/average", () => {
-        beforeAll(() => {
-            describedComponent = shallow(
-                <DisplayCooperResult distance="200" gender="female" age="23" />
-            );
-        });
+    it("and returns the data user put in", () => {
+      expect(describedComponent.find("p#cooper-message").text()).toEqual(
+        "23 y/o female running 1000 meters."
+      );
+    });
+  })
 
-        it("and returns the assessment", () => {
-            expect(describedComponent.find("p#cooper-message").text()).toEqual("Result: Average");
-                
-            
-        });
-        it("and returns the data user put in", () => {
-            expect(describedComponent.find("p#cooper-message").text()).toEqual(
-                "23 y/o female running 2000 meters."
+  describe("evaluates the correct result for female/average", () => {
+    beforeAll(() => {
+      describedComponent = shallow(
+        <DisplayCooperResult distance="2000" gender="female" age="23" />
+      );
+    });
 
-            );
-        });
-    })
+    it("and returns the assessment", () => {
+      expect(describedComponent.find("p#cooper-result").text()).toEqual("Result: Average");
+    });
+
+    it("and returns the data user put in", () => {
+      expect(describedComponent.find("p#cooper-message").text()).toEqual(
+        "23 y/o female running 2000 meters."
+      );
+    });
+  })
 });
