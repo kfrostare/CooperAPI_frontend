abc41aa9406b8b26463c7e4117ddbeeae804d23d
karolina <karolina.frostare@gmail.com>
2020-02-28 18:45:35 +0100

    adds new component called DisplayPerformanceData.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,19 @@ class App extends Component {
           </>
         );
         break;
-        case authenticated:
-          renderLogin = (
-            <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
-          );
-          performanceDataIndex = (
-            <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
-          )
-          break;
+      case authenticated:
+        renderLogin = (
+          <p>Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+        );
+        performanceDataIndex = (
+          <button
+            id="show-index"
+            onClick={() => this.setState({ renderIndex: true })}
+          >
+            Show past entries
+          </button>
+        );
+        break;
     }
     return (
       <>
@@ -59,8 +64,11 @@ class App extends Component {
           age={this.state.age}
           authenticated={this.state.authenticated}
           entrySaved={this.state.entrySaved}
-          entryHandler={() => this.setState({ entrySaved: true})}
+          entryHandler={() =>
+            this.setState({ entrySaved: true, updateIndex: true })
+          }
         />
+        {performanceDataIndex}
       </>
     );
   }
---
+++ src/components/DisplayPerformanceData.jsx
@@ -0,0 +1,4 @@
+
+//If you run the test now it will complain about not 
+//finding the text that we are looking for. Its time 
+//to create a new component now.
