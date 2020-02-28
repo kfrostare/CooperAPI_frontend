82d3576d05eca3fa7f8f7aad35e4c1b34449fe99
karolina <karolina.frostare@gmail.com>
2020-02-28 16:48:08 +0100

    creates src/modules/performanceData.js- file and populate

--- cypress/integration/userCanLogin.feature.js
+++ cypress/integration/userCanLogin.feature.js
@@ -17,7 +17,9 @@ describe("User can log in", () => {
     cy.get("#login-form").within(() => {
       cy.get("#email").type("user@mail.com");
       cy.get("#password").type("password");
-      cy.get('button').contains('Submit').click();
+      cy.get("button")
+        .contains("Submit")
+        .click();
     });
     cy.get("#message").should("contain", "Hi user@mail.com");
   });
@@ -36,8 +38,13 @@ describe("User can log in", () => {
     cy.get("#login-form").within(() => {
       cy.get("#email").type("user@mail.com");
       cy.get("#password").type("wrongpassword");
-      cy.get('button').contains('Submit').click()
+      cy.get("button")
+        .contains("Submit")
+        .click();
     });
-    cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
+    cy.get("#message").should(
+      "contain",
+      "Invalid login credentials. Please try again."
+    );
   });
 });
