66c9f351ad08810f488f07647b7c5fe2a02996f3
karolina <karolina.frostare@gmail.com>
2020-02-28 17:40:44 +0100

    updates onChangeHandler in app.jsx

--- src/modules/performanceData.js
+++ src/modules/performanceData.js
@@ -1,24 +1,27 @@
 import axios from "axios";
 
 const saveData = async (result, entryHandler) => {
-    let headers = JSON.parse(headers);
-    headers = {
-        ...headers,
-        "Content-type": "application/json",
-        Accept: "application/json"
-    };
-    try {
-        await axios.post("/performance_data",
-        {
-            performance_data: {data: {messsage:result } }
-        }, {
-            headers: headers
-        }
+  let headers = sessionStorage.getItem("credentials");
+  headers = JSON.parse(headers);
+  headers = {
+    ...headers,
+    "Content-type": "application/json",
+    Accept: "application/json"
+  };
+  try {
+    await axios.post(
+      "/performance_data",
+      {
+        performance_data: { data: { message: result } }
+      },
+      {
+        headers: headers
+      }
     );
     entryHandler();
   } catch (err) {
-      console.error(err);
-      alert("Something went wrong");
+    console.error(err);
+    alert("Something went wrong");
   }
 };
 
