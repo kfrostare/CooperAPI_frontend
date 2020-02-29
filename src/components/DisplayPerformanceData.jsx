eee68865b74424a015990bf1a992712c432fd711
karolina <karolina.frostare@gmail.com>
2020-02-29 15:57:09 +0100

    updates chart colors

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -1,66 +1,76 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { getData } from "../modules/performanceData";
-import { Line } from 'react-chartjs-2';
+import { Line } from "react-chartjs-2";
 
 class DisplayPerformanceData extends Component {
   state = {
     performanceData: null
-  }
+  };
 
   componentDidMount() {
-    this.getPerformanceData()
+    this.getPerformanceData();
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.updateIndex != prevProps.updateIndex) {
-      this.getPerformanceData()
+      this.getPerformanceData();
     }
   }
 
   async getPerformanceData() {
     let result = await getData();
-    this.setState({performanceData: result.data.entries}, () => {
+    this.setState({ performanceData: result.data.entries }, () => {
       this.props.indexUpdated();
-    })
+    });
   }
 
-  render () {
+  render() {
     let dataIndex;
 
     if (this.state.performanceData != null) {
       dataIndex = (
         <div id="index">
           {this.state.performanceData.map(item => {
-            return <div key={item.id}>{item.data.distance}m, {item.data.message}</div>
+            return (
+              <div key={item.id}>
+                {item.data.distance}m, {item.data.message}
+              </div>
+            );
           })}
         </div>
-      )
+      );
     }
-    const distances = []
-    const labels = []
+    const distances = [];
+    const labels = [];
 
     if (this.state.performanceData != null) {
       this.state.performanceData.forEach(entry => {
-        distances.push(entry.data.distance)
-        labels.push(entry.data.message)
-      })
+        distances.push(entry.data.distance);
+        labels.push(entry.data.message);
+      });
     }
 
     let dataForLineDiagram = {
-      datasets: [{
-        data: distances,
-        label: "Historical Data"
-      }],
+      datasets: [
+        {
+          data: distances,
+          label: "Historical Data",
+          fill: true, 
+          borderColor: "#A28C74",
+          backgroundColor: "#DDCCAF",
+          pointBackgroundColor: "#3D2C23"
+        }
+      ],
       labels: labels
-    }
+    };
 
     return (
       <div>
         {dataIndex}
-        <Line data={dataForLineDiagram}/>
+        <Line id="lineColor" data={dataForLineDiagram} />
       </div>
-    )
-  }      
+    );
+  }
 }
 
 export default DisplayPerformanceData;
